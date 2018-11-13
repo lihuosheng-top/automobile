0cd81b7b560786416c2fb89dc570fa896b580683
lihuosheng-top <1061525487@qq.com>
2018-11-13 13:43:53 +0800

    注册

--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -9,6 +9,7 @@ namespace app\index\controller;
 use think\Controller;
 use think\Request;
 use think\Session;
+use think\Db;
 
 
 
@@ -27,28 +28,27 @@ class Register extends Controller{
     {
         //接受验证码的手机号码
         if ($request->isPost()) {
-            $mobile = $_POST["mobile"];
-            if(preg_match("/^1[02178]{1}\d{9}$/",$mobile)) {
-                return ajax_error("请填写正确的手机号",['status'=>0]);
-            }
-            $res =  Db::name('user')->field('phone_num')->where('phone_num',$mobile)->select();
-            if($res){
-                return ajax_success('此手机号已经注册',$mobile);
-            }
-            $mobileCode = rand(100000, 999999);
-            $arr = json_decode($mobile, true);
-            $mobiles = strlen($arr);
-            if (isset($mobiles) != 11) {
-                return ajax_success("手机号码不正确",$mobile);
-            }
-            //存入session中
-            if (strlen($mobileCode)> 0) {
-                session('mobileCode',$mobileCode);
-                $_SESSION['mobile'] = $mobile;
-            }
+            $mobile = $request->only(['mobile'])['mobile'];
+            $pattern = '/^1[3456789]\d{9}$/';
+            if(preg_match($pattern,$mobile)) {
+                $res =  Db::name('user')->field('phone_num')->where('phone_num',$mobile)->select();
+                if($res){
+                    return ajax_error('此手机号已经注册',['status'=>0]);
+                }
+                $mobileCode = rand(100000, 999999);
+                $arr = json_decode($mobile, true);
+                $mobiles = strlen($arr);
+                if (isset($mobiles) != 11) {
+                    return ajax_error("手机号码不正确",$mobile);
+                }
+                //存入session中
+                if (strlen($mobileCode)> 0) {
+                    session('mobileCode',$mobileCode);
+                    $_SESSION['mobile'] = $mobile;
+                }
                 $content = "尊敬的用户，您本次验证码为{$mobileCode}，十分钟内有效";
                 $url = "http://120.26.38.54:8000/interface/smssend.aspx";
-                $post_data = array("account" => "qiche", "password" => "123qwe", "mobile" => "$mobile", "content" => $content);
+                $post_data = array("account" => "gagaliang", "password" => "123qwe", "mobile" => "$mobile", "content" => $content);
                 $ch = curl_init();
                 curl_setopt($ch, CURLOPT_URL, $url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -56,12 +56,16 @@ class Register extends Controller{
                 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
                 $output = curl_exec($ch);
                 curl_close($ch);
-            if ($output) {
-                return ajax_success("发送成功", $output);
-            } else {
-                return ajax_success("发送失败",$output);
+                if ($output) {
+                    return ajax_success("发送成功", $output);
+                } else {
+                    return ajax_success("发送失败",$output);
+                }
             }
-        }
+            }else{
+                return ajax_error("请填写正确的手机号",['status'=>0]);
+            }
+
     }
 
     /**
