71c81c2353a359b20557175417d352dc7d54f7b0
youdianku <571851168@qq.com>
2018-11-28 16:50:01 +0800

    pull

--- public/static/index/js/order_parts/order_parts_all.js
+++ public/static/index/js/order_parts/order_parts_all.js
@@ -1,4 +1,23 @@
 var app = new Vue({
     el: '#app',
-    
+    data: {
+        allDatas: ''
+    },
+    created: function(){
+        this.getData()
+    },
+    methods: {
+        getData: function(){
+            this.$http.post('ios_api_order_parts_all')
+            .then((res) => {
+                console.log(eval("(" + res.body + ")"));
+                this.allDatas = eval("(" + res.body + ")").data;
+            }, (res) => {
+                console.log('error');
+            })
+        }
+    },
+    computed: {
+        
+    }
 })
