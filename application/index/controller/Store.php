81f75604936199ad701c1f8947499c40b6157c6b
chenxu11oopp <you@example.com>
2018-11-29 10:19:44 +0800

    chenxu

--- application/index/controller/Store.php
+++ application/index/controller/Store.php
@@ -31,18 +31,18 @@ class Store extends Controller{
      */
     public function league(Request $request){
 
-        if($request->isPost()){
+        if($request->isPost()) {
             $user_id = Session::get("user");
-            $user = db("user")->where("id",$user_id)->field("phone_num,sex,real_name")->select();
-            $roles = db("role")->where("status","1")->field("id,name")->select();
-            foreach ($roles as $key=>$value){
-                if($value["id"] == 2){
+            $user = db("user")->where("id", $user_id)->field("phone_num,sex,real_name")->select();
+            $roles = db("role")->where("status", "1")->field("id,name")->select();
+            foreach ($roles as $key => $value) {
+                if ($value["id"] == 2) {
                     unset($roles[$key]);
                 }
             }
-            if($roles){
-                return ajax_success("获取成功",array("user"=>$user,"roles"=>$roles));
-            }else{
+            if ($roles) {
+                return ajax_success("获取成功", array("user" => $user, "roles" => $roles));
+            } else {
                 return ajax_error("获取失败");
             }
         }
