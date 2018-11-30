e2a2362d59611db04e23e45303742abc1289ae5c
chenxu11oopp <you@example.com>
2018-11-30 10:11:05 +0800

    chenxu

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -13,10 +13,24 @@ class Index extends Controller
      * 首页
      * 陈绪
      */
-    public function index()
+    public function index(Request $request)
     {
 
-        
+        if($request->isPost()) {
+            $user_id = Session::get("user");
+            if (!empty($user_id)) {
+                $user_car = db("user_car")->where("user_id", $user_id)->where("status", 1)->select();
+                if ($user_car) {
+                    return ajax_success("获取成功", $user_car);
+                } else {
+                    return ajax_error("获取失败");
+                }
+
+            } else {
+                exit(json_encode(array("status" => 2, "info" => "请登录")));
+            }
+        }
+
         return view("index");
     }
 
