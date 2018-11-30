7a2c9307d30f3d13df3e8390a998120cfde34d1c
lihuosheng-top <1061525487@qq.com>
2018-11-30 16:32:08 +0800

    app配件商订单多商户订单列表

--- application/index/controller/OrderParts.php
+++ application/index/controller/OrderParts.php
@@ -75,13 +75,22 @@ class OrderParts extends Controller{
                     };
                     if (!empty($order_undate)) {
                         foreach ($order_undate['info'] as $i => $j) {
+                            if(!empty($j)){
                                 $end_info[$i]['info'] = $j;
+                            }
+
                         }
                         foreach ($order_undate['store_name'] as $i => $j) {
+                            if(!empty($j)){
                                 $end_info[$i]['store_name'] = $j;
+                            }
+
                         }
                         foreach ($order_undate['status'] as $i => $j) {
+                            if(!empty($j)){
                                 $end_info[$i]['status'] = $j;
+                            }
+
                         }
                     }
                     if(!empty($data_infomation)){
