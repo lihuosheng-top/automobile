6b4d4d1df58eceec10be9ab8c9f5076f31bfa342
youdianku <571851168@qq.com>
2018-11-30 09:50:29 +0800

    push

--- public/static/index/js/love_list.js
+++ public/static/index/js/love_list.js
@@ -23,7 +23,8 @@ $.ajax({
         console.log(res);
         var str = '';
         $.each(res.data, function(idx, val){
-            str += `<div class="car-box">
+            if(val.status !== 1){
+                str += `<div class="car-box">
                         <div class="car-info-top">
                             <div class="logo"><img src="static/index/image/aodi.png"></div>
                             <div class="car-info">
@@ -36,10 +37,27 @@ $.ajax({
                             <button class="delete-btn">删除</button>
                         </div>
                     </div>`
+            }else{
+                str += `<div class="car-box">
+                        <div class="car-info-top">
+                            <div class="logo"><img src="static/index/image/aodi.png"></div>
+                            <div class="car-info">
+                                <p>`+val.brand+` `+val.series+`</p>
+                                <p>`+val.displacement+` `+val.production_time+`</p>
+                            </div>
+                        </div>
+                        <div class="car-btn-bot">
+                            <button class="set-default on-default" id="`+val.id+`">设为默认</button>
+                            <button class="delete-btn">删除</button>
+                        </div>
+                    </div>`
+            }
         })
         $('.box-wrap').append(str);
         $('.set-default').click(function(){
             var id = $(this)[0].id;
+            $('.box-wrap').find('.set-default').removeClass('on-default');
+            $(this).addClass('on-default');
             $.ajax({
                 url: 'love_status',
                 type: 'POST',
@@ -50,6 +68,63 @@ $.ajax({
                 },
                 success: function(res){
                     console.log(res);
+                    if(res.status === 1){
+                        layer.open({
+                            style: 'bottom:100px;',
+                            type: 0,//弹窗类型 0表示信息框，1表示页面层，2表示加载层
+                            skin: 'msg',
+                            content: res.info,
+                            time: 1.5
+                        })
+                    }else{
+                        layer.open({
+                            style: 'bottom:100px;',
+                            type: 0,//弹窗类型 0表示信息框，1表示页面层，2表示加载层
+                            skin: 'msg',
+                            content: res.info,
+                            time: 1.5
+                        })
+                    }
+                },
+                error: function(){
+                    console.log('error');
+                }
+            })
+        })
+
+        // 删除
+        $('.delete-btn').click(function(){
+            var id = $(this).siblings()[0].id;
+            $.ajax({
+                url: 'love_del',
+                type: 'POST',
+                dataType: 'JSON',
+                data: {
+                    'id': id
+                },
+                success: function(res){
+                    console.log(res);
+                    if(res.status === 1){
+                        layer.open({
+                            style: 'bottom:100px;',
+                            type: 0,//弹窗类型 0表示信息框，1表示页面层，2表示加载层
+                            skin: 'msg',
+                            content: res.info,
+                            time: 1.5
+                        })
+                        alert(111)
+                        setTimeout(function(){
+                            window.location.reload();
+                        }, 2000);
+                    }else{
+                        layer.open({
+                            style: 'bottom:100px;',
+                            type: 0,//弹窗类型 0表示信息框，1表示页面层，2表示加载层
+                            skin: 'msg',
+                            content: res.info,
+                            time: 1.5
+                        })
+                    }
                 },
                 error: function(){
                     console.log('error');
