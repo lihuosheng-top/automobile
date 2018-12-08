ec7139968238790714307d82cfc9a7a2b0f7028f
lihuosheng-top <1061525487@qq.com>
2018-12-08 16:39:14 +0800

    laq

--- application/route.php
+++ application/route.php
@@ -73,6 +73,7 @@ Route::group("",[
     "phone_edit"=>"index/My/phone_edit",        //手机号码修改
     "true_name"=>"index/My/true_name",          //真实姓名
     "member_information_update"=>"index/My/member_information_update",//个人信息用户个人信息更新（头像,真实姓名，昵称，性别）
+    "member_information_data"=>"index/My/member_information_data",//个人信息用户个人信息返回（头像,真实姓名，昵称，性别）
     "my_integral"=>"index/My/integral",            //我的积分
     "setting"=>"index/My/setting",              //设置
     "my_nickname"=>"index/My/nickname",         //昵称
