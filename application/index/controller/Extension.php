5de2f14e6656c75639050ae83edf24f56d7da017
lihuosheng-top <1061525487@qq.com>
2018-12-11 11:29:54 +0800

    推广

--- application/index/controller/Extension.php
+++ application/index/controller/Extension.php
@@ -13,6 +13,13 @@ use think\Controller;
 
 class Extension extends  Controller{
 
+    /**
+     **************李火生*******************
+     * @param Request $request
+     * Notes:我要推广页面
+     **************************************
+     * @return \think\response\View
+     */
     public function spread_index(){
         return view("spread_index");
     }
