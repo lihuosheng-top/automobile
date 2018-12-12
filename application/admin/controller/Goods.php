1e7f53fdebc1747c80b481258581bf18805ac8ca
chenxu11oopp <you@example.com>
2018-12-12 18:20:17 +0800

    cehnxu

--- application/admin/controller/Goods.php
+++ application/admin/controller/Goods.php
@@ -836,6 +836,7 @@ class Goods extends Controller{
 
         $result = $notify->GetPayUrl($input);
         $url2 = $result["code_url"];
+        halt($url2);
 
         return view("WeiAlpay_code",["url2"=>$url2]);
 
