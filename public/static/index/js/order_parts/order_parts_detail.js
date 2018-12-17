844cb79a7255fb723feaae95ac9993708532ad24
youdianku <571851168@qq.com>
2018-12-17 13:57:41 +0800

    push

--- public/static/index/js/order_parts/order_parts_detail.js
+++ public/static/index/js/order_parts/order_parts_detail.js
@@ -9,6 +9,46 @@ function timetrans(date){
     // var s = (date.getSeconds() <10 ? '0' + date.getSeconds() : date.getSeconds());
     return Y+M+D+h+m;
 }
+// 倒计时
+function countDown(id, endTime, storeId, orderNum, reason){
+    var nowDate = new Date();
+    var endDate = new Date(endTime * 1000);
+    // 相差总秒数
+    var totalSecond = parseInt((endDate - nowDate) / 1000);
+    if(totalSecond > 0){
+        // console.log(totalSecond)
+        // 小时
+        var hours = Math.floor(totalSecond / 3600);
+        // console.log(hours);
+        // 分钟
+        var minutes = Math.floor((totalSecond % 3600) / 60);
+        // console.log(minutes);
+        // 秒
+        // var seconds = Math.floor((totalSecond % 3600) % 60);
+        // console.log(seconds);
+        document.getElementById(id).innerText = '剩'+hours+'小时'+minutes+'分自动关闭';
+        setTimeout(function(){
+            countDown(id, endTime, storeId, orderNum, reason);
+        }, 1000)
+    }else{
+        $.ajax({
+            url: 'order_parts_detail_cancel',
+            type: 'POST',
+            dataType: 'JSON',
+            data: {
+                'store_id': storeId,
+                'parts_order_number': orderNum,
+                'Cancel_order_description': reason
+            },
+            success: function(res){
+                console.log(res);
+            },
+            error: function(){
+                console.log('error');
+            }
+        })
+    }
+}
 
 $.ajax({
     url: 'order_parts_detail',
@@ -22,6 +62,7 @@ $.ajax({
             statusTxt = `待付款`;
             $('.cancel-order-btn').add('.to-payment-btn').show();
             $('.time-count-down').show();
+            countDown('time-count-down', val.normal_future_time, val.store_id, val.parts_order_number, '超过规定时间未付款，系统自动关闭订单');
         }else if(val.status === 2 || val.status === 3 || val.status === 4 || val.status === 5){
             statusTxt = `待收货`;
             $('.check-logistics-btn').add('.conf-receipt-btn').show();
