394afdab1293e5ece6081e186ce4cdf8b2365a78
chenxu11oopp <you@example.com>
2018-12-17 20:21:09 +0800

    chjenxu

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -56,12 +56,10 @@ class Index extends Controller
     {
         //扫码支付，接收微信请求
 
-        $goods_id = Session::get("goods_id");
-        halt($goods_id);
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
         $xml_data = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $val = json_decode(json_encode($xml_data),true);
-        file_put_contents(EXTEND_PATH."lib/data/data.txt",$xml);
+        file_put_contents(EXTEND_PATH."lib/data/data.txt",$val);
     }
 
 
