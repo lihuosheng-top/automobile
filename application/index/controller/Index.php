75efd932ab70e7c61cff2dc841fceed97f1c2875
chenxu11oopp <you@example.com>
2018-12-18 11:40:08 +0800

    cehnxu

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -57,14 +57,13 @@ class Index extends Controller
     public function saoma_callback(Request $request)
     {
         //扫码支付，接收微信请求;
-        $goods_id = Session::get("goods_id");
-        file_put_contents(EXTEND_PATH . "lib/data/data.txt", $goods_id);
-        exit();
+
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
         $xml_data = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $val = json_decode(json_encode($xml_data), true);
-        //$goods_id = Session::get("goods_id");
-
+        $goods_id = Session::get("goods_id");
+        file_put_contents(EXTEND_PATH . "lib/data/data.txt", $val);
+        exit();
         if(!empty($val)){
 
         }
