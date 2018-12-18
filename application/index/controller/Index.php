32359f4763d07098ba1d3ea48f5c9f5c1e67d21f
chenxu11oopp <you@example.com>
2018-12-18 11:25:14 +0800

    cehnxu

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;
 
+use think\Config;
 use think\Controller;
 use think\Request;
 use think\Session;
@@ -61,7 +62,7 @@ class Index extends Controller
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
         $xml_data = simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA);
         $val = json_decode(json_encode($xml_data), true);
-        $goods_id = Session::get("goods_id");
+        $goods_id = Config::get("goods_id");
         file_put_contents(EXTEND_PATH . "lib/data/data.txt", $goods_id);
         exit();
         if(!empty($val)){
