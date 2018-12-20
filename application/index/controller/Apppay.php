daa0e36e5368abf320614070af23e70f2ab43828
lihuosheng-top <1061525487@qq.com>
2018-12-20 17:29:48 +0800

    laqu

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -331,7 +331,7 @@ class Apppay extends Controller
                     $datas["money_status"] =1; //到款状态
                     $datas["img_url"] ="index/image/alipay.png"; //描述
                     Db::name("recharge_reflect")->insert($datas);//插到记录
-                    $list =Db::name("recharge_setting")->field("send_money")->where("recharge_full",$recharge_record_data["recharge_money"])->find();
+                    $list =Db::name("recharge_setting")->field("send_money")->where("recharge_full",0.01)->find();
                     if(!empty($list)){
                         $datas["operation_amount"] =$recharge_record_data["recharge_money"]+$list['send_money']; //操作金额
                         $datas["recharge_describe"] ="充值".$recharge_record_data["recharge_money"]."元,送了".$list['send_money']; //描述
