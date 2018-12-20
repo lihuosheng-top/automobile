a41bd450f0657934ccd4f81882c2c5254939a68b
lihuosheng-top <1061525487@qq.com>
2018-12-20 17:57:33 +0800

    充值

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -330,13 +330,19 @@ class Apppay extends Controller
                     $datas["pay_type_content"] =$recharge_record_data["pay_type_name"]; //支付方式
                     $datas["money_status"] =1; //到款状态
                     $datas["img_url"] ="index/image/alipay.png"; //描述
-                    $list =Db::name("recharge_setting")->field("send_money")->where("recharge_full","0.01")->find();
-                    if(!empty($list)){
-                        $datas["operation_amount"] =$recharge_record_data["recharge_money"]+$list['send_money']; //操作金额
-                        $datas["recharge_describe"] ="充值".$recharge_record_data["recharge_money"]."元,送了".$list['send_money']; //描述
+                    $list =Db::name("recharge_setting")->field("recharge_full,send_money")->select();
+                    $lists =null;
+                    foreach($list as $k=>$v){
+                        if($v["recharge_full"] =="0.01"){
+                            $lists =$v["send_money"];
+                        }
+                    }
+                    if(!empty($lists)){
+                        $datas["operation_amount"]=$recharge_record_data["recharge_money"]+$lists; //操作金额
+                        $datas["recharge_describe"] ="充值".$recharge_record_data["recharge_money"]."元,送了".$lists; //描述
                         Db::name("recharge_reflect")->insert($datas);//插到记录
                         $user_wallet =Db::name("user")->field("user_wallet")->where("id",$user_id)->find();
-                        Db::name("user")->where("id",$user_id)->update(["user_wallet"=>$user_wallet["user_wallet"]+$recharge_record_data["recharge_money"]+ $list["send_money"]]);
+                        Db::name("user")->where("id",$user_id)->update(["user_wallet"=>$user_wallet["user_wallet"]+$recharge_record_data["recharge_money"]+ $lists]);
                     }else{
                         $datas["operation_amount"] =$recharge_record_data["recharge_money"]; //操作金额
                         $datas["recharge_describe"] ="充值".$recharge_record_data["recharge_money"]."元"; //描述
