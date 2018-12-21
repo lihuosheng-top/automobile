e3f1f3a2630baf2db39f529afc3a0a99ba6d68ba
lihuosheng-top <1061525487@qq.com>
2018-12-21 17:54:25 +0800

    laqu

--- application/common.php
+++ application/common.php
@@ -788,5 +788,48 @@ function show_service_order_status($status){
 }
 
 
+/**
+ **************李火生*******************
+ * @param Request $request
+ * Notes:生成我的邀请码
+ **************************************
+ * @param $user_id
+ * @return string
+ */
+function createCode($user_id) {
+    static $source_string = 'E5FCDG3HQA4B1NOPIJ2RSTUV67MWX89KLYZ';
+    $num = $user_id;
+    $code = '';
+    while ( $num > 0) {
+        $mod = $num % 35;
+        $num = ($num - $mod) / 35;
+        $code = $source_string[$mod].$code;
+    }
+    if(empty($code[3]))
+        $code = str_pad($code,6,'0',STR_PAD_LEFT);
+   return $code;
+}
+
+/**
+ **************李火生*******************
+ * @param Request $request
+ * Notes:邀请码解析
+ **************************************
+ * @param $code
+ * @return bool|int
+ */
+function qr_decode($code) {
+    static $source_string = 'E5FCDG3HQA4B1NOPIJ2RSTUV67MWX89KLYZ';
+    if (strrpos($code, '0') !== false)
+        $code = substr($code, strrpos($code, '0')+1);
+    $len = strlen($code);
+    $code = strrev($code);
+    $num = 0;
+    for ($i=0; $i < $len; $i++) {
+        $num += strpos($source_string, $code[$i]) * pow(35, $i);
+    }
+    return $num;
+}
+
 
 
