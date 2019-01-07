f3a40a4d773c1900ae58c831951183ca9e1c47eb
chenxu11oopp <you@example.com>
2019-01-07 14:49:03 +0800

    chenxu

--- application/index/controller/Service.php
+++ application/index/controller/Service.php
@@ -8,6 +8,7 @@
 namespace app\index\controller;
 use think\Controller;
 use think\Request;
+use think\Session;
 
 class Service extends Controller{
 
@@ -16,8 +17,27 @@ class Service extends Controller{
      * 选择服务类型
      * 陈绪
      */
-    public function type_index(){
+    public function type_index(Request $request){
 
+        if($request->isPost()) {
+            $order_id = Session::get("order_id");
+            $order = db("order_parts")->where("id",$order_id)->select();
+            $service = [];
+            foreach ($order as $key=>$value){
+                $service[$key]["order_images"] = $value["goods_image"];
+                $service[$key]["order_money"] = $value["order_amount"];
+                $service[$key]["order_name"] = $value["parts_goods_name"];
+                $service[$key]["order_describe"] = $value["goods_describe"];
+                $service[$key]["order_id"] = $order_id;
+            }
+            if($service){
+                Session::delete("order_id");
+                return ajax_success('获取成功',$service);
+            }else{
+                return ajax_error("获取失败");
+            }
+
+        }
         return view("type_index");
 
     }
@@ -41,6 +61,7 @@ class Service extends Controller{
                 $service[$key]["order_describe"] = $value["goods_describe"];
                 $service[$key]["order_id"] = $order_id;
             }
+            Session::set("order_id",$order_id);
             if($service){
                 return ajax_success('获取成功',$service);
             }else{
