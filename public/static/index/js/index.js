14d1fd2b48b6667a157297bebee31192cd925574
youdianku <571851168@qq.com>
2019-01-08 10:45:03 +0800

    pull

--- public/static/index/js/index.js
+++ public/static/index/js/index.js
@@ -556,20 +556,6 @@ $('.gec-back').click(function(){
     $('.wrapper').show();
 })
 
-function callAndroid(){
-    Android.getLocation();
-}
-function getAndroid(e){
-    setCookie('district', e.district, 7);
-    if(getCookie('district')){
-        $('.curr_city').text(getCookie('district'));
-    }
-    $('.geclocation-pop').hide();
-    $('.wrapper').show();
-}
-$('.gec-curr-box').click(function(){
-    callAndroid();
-})
 // var map = new AMap.Map('container', {
 //     zoom: 12, //级别
 //     center: [114.07, 22.62]
@@ -774,19 +760,3 @@ function mySwiper(){
         }
     })
 }
-//设置cookie
-function setCookie(name, value, day) {
-    var date = new Date();
-    date.setDate(date.getDate() + day);
-    document.cookie = name + '=' + value + ';expires=' + date;
-};
-//获取cookie
-function getCookie(name) {
-    var reg = RegExp(name + '=([^;]+)');
-    var arr = document.cookie.match(reg);
-    if (arr) {
-        return arr[1];
-    } else {
-        return '';
-    }
-};
