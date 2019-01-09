db6683dcce3414fa0ee665591d5bd2b185348cc7
lihuosheng-top <1061525487@qq.com>
2019-01-09 16:37:10 +0800

    laqu

--- application/route.php
+++ application/route.php
@@ -254,10 +254,12 @@ Route::group("",[
     /*配件商品*/
     "goods_list"=>"index/Classify/goods_list",//商品列表
     "goods_detail"=>"index/Classify/goods_detail",//商品详情
+
+    /*TODO:商品详情页面信息开始*/
     "go_to_store"=>"index/Classify/go_to_store",//商品详情页面进店信息返回
     "may_like_goods"=>"index/Classify/may_like_goods",//商品详情页面你可能还喜欢
     "goods_evaluate_return"=>"index/Classify/goods_evaluate_return",//商品详情页面商品详情页面的评价数据
-
+    /*TODO:商品详情页面信息结束*/
 
 
     /*配件商城*/
