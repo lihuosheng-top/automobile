08cbcc3785449e3c6e86a2c090f4cd2044745e51
chenxu11oopp <you@example.com>
2019-01-14 11:46:52 +0800

    chenxu

--- application/config.php
+++ application/config.php
@@ -141,7 +141,7 @@ return [
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         /*"__AdminCss__"=>"/static/admin/static"*/
-        "__UPLOADS__"=>"/uploads",
+        "__UPLOADS__"=>"/automobile/public/uploads",
         "__UEDITOR__"=>"/ueditor",
         "__USERIMG__"=>"/userimg",
     ],
