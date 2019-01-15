058b361eed1d9f5cbb49e0c563d412c3c16c59c7
youdianku <571851168@qq.com>
2019-01-15 18:06:12 +0800

    push

--- public/static/index/js/order_parts/order_parts_detail.js
+++ public/static/index/js/order_parts/order_parts_detail.js
@@ -43,7 +43,7 @@ function countDown(id, endTime, storeId, orderNum, reason){
                 }
             })
         })().then(function(){
-            $.ajax({
+            return $.ajax({
                 url: 'order_parts_save_record',
                 type: 'POST',
                 dataType: 'JSON',
@@ -51,12 +51,101 @@ function countDown(id, endTime, storeId, orderNum, reason){
                     'parts_order_number': orderNum,
                     'store_id': storeId,
                     'status': 9
-                },
-                success: function(res){
-                    console.log(res);
-                    location.reload();
                 }
             })
+        }).then(function(){
+            return $.ajax({
+                url: 'order_parts_detail',
+                type: 'POST',
+                dataType: 'JSON'
+            })
+        }).then(function(res){
+            console.log(res);
+            var val = res.data;
+            var statusTxt = '';
+            if(val.status === 1){
+                statusTxt = `待付款`;
+                $('.cancel-order-btn')
+                    .add('.to-payment-btn')
+                    .show();
+                $('.time-count-down').show();
+                countDown('time-count-down', val.normal_future_time, val.store_id, val.parts_order_number, '超过规定时间未付款，系统自动关闭订单');
+            }else if(val.status === 2 || val.status === 3 || val.status === 4 || val.status === 5){
+                statusTxt = `待收货`;
+                $('.check-logistics-btn')
+                    .add('.conf-receipt-btn')
+                    .show();
+            }else if(val.status === 6 || val.status === 7){
+                statusTxt = `待评价`;
+                $('.evaluation-btn')
+                    .add('.del-order-btn')
+                    .add('.return-goods')
+                        .show();
+            }else if(val.status === 8){
+                statusTxt = `已完成`;
+            }else if(val.status === 9 || val.status === 10){
+                statusTxt = `已取消`;
+                $('.del-order-btn').show();
+            }else if(val.status === 11 || val.status === 13 || val.status === 15){
+                statusTxt = `退货中`;
+                // $('.del-order-btn').show();
+            }else if(val.status === 12){
+                statusTxt = `已退货`;
+                $('.del-order-btn').show();
+            }else if(val.status === 14){
+                statusTxt = `拒绝退货`;
+            }
+            // 状态值
+            $('.status').text(statusTxt);
+            // 订单编号
+            $('.order-num span').text(val.parts_order_number);
+            // 收货人
+            $('.user-name span').text(val.harvester);
+            // 电话
+            $('.user-phone').text(val.harvest_phone_num);
+            // 配送地址
+            $('.address-txt').text(val.harvester_address);
+            // 店铺名
+            $('.order-shop-namp').text(val.store_name);
+            $('.order-shop-namp').attr('id', val.store_id);
+            $('.order-shop-namp').data({'data-status': val.status});
+            // 订单商品
+            var str = '';
+            $.each(val.info, function(idx, val){
+                str += `<div class="order-goods-detail" id="`+val.id+`">
+                            <div class="order-goods-img">
+                                <img src="uploads/`+val.goods_image+`">
+                            </div>
+                            <div class="order-info-box">
+                                <p class="order-goods-p txt-hid-two">`+val.parts_goods_name+`</p>
+                                <p class="order-selling-point txt-hid-two">`+val.goods_describe+`</p>
+                                <div class="unit-price-quantity">
+                                    <p class="unit-price-p">￥`+val.goods_money+`</p>
+                                    <p class="quantity-p">×`+val.order_quantity+`</p>
+                                    <p style="display:`+(val.status==11?'block': 
+                                                        (val.status==13?'block':
+                                                        (val.status==15?'block':'none')))+`;">退货中</p>
+                                    <p style="display:`+(val.status==12?'block':'none')+`;">已退货</p>
+                                    <button class="return-goods" style="display:`+(val.status==7?'block':'none')+`;">退货</button>
+                                </div>
+                            </div>
+                        </div>`
+            })
+            $('.order-shop-box').after(str);
+            // 商品总额
+            $('.total-box span').text(val.all_goods_pays);
+            // 抵扣金额
+            $('.discount-box span').text(val.integral_deductible);
+            // 买家留言
+            $('.leave-message').text(val.buy_message);
+            // 需付款
+            $('.pay-amount-span span').text(val.all_order_real_pay);
+            // 创建时间
+            $('.create-time span').text(timetrans(val.create_time));
+            // 支付时间
+            if(val.pay_time !== null){
+                $('.create-time').after('<p class="pay-time">支付时间：<span>'+timetrans(val.pay_time)+'</span></p>')
+            }
         })
     }
 }
@@ -362,21 +451,21 @@ $.ajax({
     }
 })
 // 选择支付方式
-$('.widway').click(function(){
-    $('.alipay-pop').animate({'bottom': '-100%'});
-    $('.chose-payment').animate({'bottom': '0'});
-})
-function backMethod(){
-    $('.alipay-pop').animate({'bottom': '0'});
-    $('.chose-payment').animate({'bottom': '-100%'});
-}
-$('.method-div').click(function(){
-    $(this).siblings().find('.check-a').removeClass('icon-check');
-    $(this).find('.check-a').addClass('icon-check');
-    var paymentMethod = $(this).find('p').text();
-    $('.widway').val(paymentMethod);
-    backMethod();
-})
-$('.chose-payment-back').click(function(){
-    backMethod();
-})
+// $('.widway').click(function(){
+//     $('.alipay-pop').animate({'bottom': '-100%'});
+//     $('.chose-payment').animate({'bottom': '0'});
+// })
+// function backMethod(){
+//     $('.alipay-pop').animate({'bottom': '0'});
+//     $('.chose-payment').animate({'bottom': '-100%'});
+// }
+// $('.method-div').click(function(){
+//     $(this).siblings().find('.check-a').removeClass('icon-check');
+//     $(this).find('.check-a').addClass('icon-check');
+//     var paymentMethod = $(this).find('p').text();
+//     $('.widway').val(paymentMethod);
+//     backMethod();
+// })
+// $('.chose-payment-back').click(function(){
+//     backMethod();
+// })
