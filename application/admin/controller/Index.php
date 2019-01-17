a24f0052e16297df4b91433bfcf09298a1fa051a
chenxu11oopp <you@example.com>
2019-01-17 11:30:07 +0800

    laqu

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -11,6 +11,8 @@ use think\Controller;
 use think\Config;
 use think\captcha\Captcha;
 use think\Request;
+use think\Session;
+
 class Index extends Controller{
 
 
@@ -22,6 +24,29 @@ class Index extends Controller{
     public function index(Request $request){
         $menu_list = Config::get("menu_list");
         $admin = Config::get("admin");
+        $admin_id = Session::get("user_id");
+        $admin_role = db("admin")->where("id", $admin_id)->field("role_id")->find();
+        if ($admin_role["role_id"] == 2) {
+            $goods_year = db("goods")->field("goods_year_id,id")->select();
+            $time = date("Y-m-d");
+            foreach ($goods_year as $key => $value) {
+                $year = db("year")->where("id", $value["goods_year_id"])->value("year");
+                $date = date("Y-m-d", strtotime("+$year year"));
+                if ($time == $date) {
+                    $bool = db("goods")->where("id", $value["id"])->update(["goods_status" => 0, "putaway_status" => null]);
+                }
+            }
+        }else{
+            $goods_year = db("goods")->field("goods_year_id,id")->select();
+            $time = date("Y-m-d");
+            foreach ($goods_year as $key => $value) {
+                $year = db("year")->where("id", $value["goods_year_id"])->value("year");
+                $date = date("Y-m-d", strtotime("+$year year"));
+                if ($time == $date) {
+                    $bool = db("goods")->where("id", $value["id"])->update(["goods_status" => 0, "putaway_status" => null]);
+                }
+            }
+        }
         return view("index",["menu_list"=>Config::get("menu_list"),"admin"=>$admin]);
     }
 
