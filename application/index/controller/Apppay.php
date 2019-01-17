6837fe819a9727eb01aa47e6c4d857346ff87728
lihuosheng-top <1061525487@qq.com>
2019-01-17 10:39:37 +0800

    laqu

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -15,7 +15,7 @@ class Apppay extends Controller
 {
     /**
      **************李火生*******************
-     * 配件商订单异步处理(支付宝IOS对接)（只对ios有用）
+     * TODO:配件商订单异步处理(支付宝IOS对接)（只对ios有用）
      **************************************
      */
     public function notifyurl()
@@ -31,33 +31,35 @@ class Apppay extends Controller
         $trade_status = input('trade_status');
         if ($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS' || $trade_status =="Success") {
             $data['status'] = 2;
+            $data["trade_no"] =$trade_no;
             $condition['parts_order_number'] = $out_trade_no;
-//            $select_data = Db::name('order_parts')->where($condition)->select();
-//            foreach ($select_data as $key => $val) {
-            $result = Db::name('order_parts')->where($condition)->update($data);//修改订单状态,支付宝单号到数据库
+            $select_data = Db::name('order_parts')->where($condition)->select();
+            foreach ($select_data as $key => $val) {
+                $result = Db::name('order_parts')->where("parts_order_number", $val["parts_order_number"])->update($data);//修改订单状态,支付宝单号到数据库
+            }
             if ($result >0) {
-//                $parts =Db::name("order_parts")
-//                    ->field("parts_goods_name")
-//                    ->where("parts_order_number",$out_trade_no)
-//                    ->select();
-//                foreach($parts as $ks=>$vs){
-//                    $titles[] = $vs["parts_goods_name"];
-//                }
-//                $title =implode("",$titles);
-//                $money =Db::name("order_parts")->where("parts_order_number",$out_trade_no)->sum("order_real_pay");
-//                $datas["user_id"] =$parts[0]["user_id"]; //用户ID
-//                $datas["wallet_operation"] = -$money; //消费金额
-//                $datas["wallet_type"] = -1; //消费操作(1入，-1出)
-//                $datas["operation_time"] = date("Y-m-d H:i:s"); //操作时间
-//                $datas["wallet_remarks"] = "订单号：".$_GET['out_trade_no']."，支付宝消费".$money; //消费备注
-//                $datas["wallet_img"] = "index/image/alipay.png"; //图标
-//                $datas["title"] = $title; //标题（消费内容）
-//                Db::name("wallet")->insert($datas);
+                $parts =Db::name("order_parts")
+                    ->field("parts_goods_name")
+                    ->where("parts_order_number",$out_trade_no)
+                    ->select();
+                foreach($parts as $ks=>$vs){
+                    $titles[] = $vs["parts_goods_name"];
+                }
+                $title =implode("",$titles);
+                $money =Db::name("order_parts")->where("parts_order_number",$out_trade_no)->sum("order_real_pay");
+                $datas["user_id"] =$parts[0]["user_id"]; //用户ID
+                $datas["wallet_operation"] = -$money; //消费金额
+                $datas["wallet_type"] = -1; //消费操作(1入，-1出)
+                $datas["operation_time"] = date("Y-m-d H:i:s"); //操作时间
+                $datas["wallet_remarks"] = "订单号：".$_GET['out_trade_no']."，支付宝消费".$money; //消费备注
+                $datas["wallet_img"] = "index/image/alipay.png"; //图标
+                $datas["title"] = $title; //标题（消费内容）
+                Db::name("wallet")->insert($datas);
                 return ajax_success('支付成功', ['status' => 1]);
             } else {
                 return ajax_error('验证失败了', ['status' => 0]);
             }
-        } else {
+        }else {
             return ajax_error('验证失败', ['status' => 0]);
         }
     }
