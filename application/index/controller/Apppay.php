d6a30a4fccae8adaec326c9ad6a9271cccaf8d10
lihuosheng-top <1061525487@qq.com>
2019-01-17 10:46:02 +0800

    laqu

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -297,27 +297,25 @@ class Apppay extends Controller
             $data['trade_no'] = $trade_no;//支付宝交易号
             $condition['service_order_number'] = $out_trade_no;
             $result = Db::name('order_service')->where($condition)->update($data);//修改订单状态,支付宝单号到数据库
-            $this->redirect('index/OrderService/order_service_wait_deliver');
             if ($result) {
                 //进行钱包消费记录
-//                $parts =Db::name("order_service")
-//                    ->field("service_goods_name")
-//                    ->where($condition)
-//                    ->select();
-//                foreach($parts as $ks=>$vs){
-//                    $titles[] = $vs["parts_goods_name"];
-//                }
-//                $title =implode("",$titles);
-//                $money =$parts[0]["service_real_pay"];//金额
-//                $datas["user_id"] =$parts[0]["user_id"]; //用户ID
-//                $datas["wallet_operation"] = -$money; //消费金额
-//                $datas["wallet_type"] = -1; //消费操作(1入，-1出)
-//                $datas["operation_time"] = date("Y-m-d H:i:s"); //操作时间
-//                $datas["wallet_remarks"] = "订单号：".$_GET['out_trade_no']."，支付宝消费".$money; //消费备注
-//                $datas["wallet_img"] = "index/image/alipay.png"; //图标
-//                $datas["title"] = $title; //标题（消费内容）
-//                Db::name("wallet")->insert($datas);
-//                $this->redirect('index/OrderService/order_service_wait_deliver');
+                $parts =Db::name("order_service")
+                    ->field("service_goods_name")
+                    ->where($condition)
+                    ->select();
+                foreach($parts as $ks=>$vs){
+                    $titles[] = $vs["parts_goods_name"];
+                }
+                $title =implode("",$titles);
+                $money =$parts[0]["service_real_pay"];//金额
+                $datas["user_id"] =$parts[0]["user_id"]; //用户ID
+                $datas["wallet_operation"] = -$money; //消费金额
+                $datas["wallet_type"] = -1; //消费操作(1入，-1出)
+                $datas["operation_time"] = date("Y-m-d H:i:s"); //操作时间
+                $datas["wallet_remarks"] = "订单号：".$_GET['out_trade_no']."，支付宝消费".$money; //消费备注
+                $datas["wallet_img"] = "index/image/alipay.png"; //图标
+                $datas["title"] = $title; //标题（消费内容）
+                Db::name("wallet")->insert($datas);
                 return ajax_success('支付成功', ['status' => 1]);
             } else {
                 return ajax_error('验证失败了', ['status' => 0]);
