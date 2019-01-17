1a2f549eeaba75371dd7dee8de516f978969c09a
lihuosheng-top <1061525487@qq.com>
2019-01-17 15:54:28 +0800

    配件商，服务商，充值pay

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -605,7 +605,6 @@ class Apppay extends Controller
         $trade_status = input('trade_status');
         if ($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS' || $trade_status =="Success") {
             $data['status'] = 1;
-            $data["trade_no"] =$trade_no;
             $data['pay_type_content'] = "支付宝";//支付宝交易号
             $condition['recharge_order_number'] = $out_trade_no;
             $select_data = Db::name('recharge_record')->where($condition)->select();
