a0c43c5aa6bf46d1e4cacbd0973b7d4d91ed7948
lihuosheng-top <1061525487@qq.com>
2019-01-18 15:40:55 +0800

    laqu

--- public/static/index/js/order_parts/order_parts_firm_order.js
+++ public/static/index/js/order_parts/order_parts_firm_order.js
@@ -112,28 +112,6 @@ $('.deduction').click(function () {
     $('.discount_pop').show();
     $('.place-order-pop').hide();
 })
-
-// 选择支付方式
-$('.widway').click(function(){
-    $('.alipay-pop').animate({'bottom': '-100%'});
-    $('.chose-payment').animate({'bottom': '0'});
-})
-function backMethod(){
-    $('.alipay-pop').animate({'bottom': '0'});
-    $('.chose-payment').animate({'bottom': '-100%'});
-}
-$('.method-div').click(function(){
-    $(this).siblings().find('.check-a').removeClass('icon-check');
-    $(this).find('.check-a').addClass('icon-check');
-    var paymentMethod = $(this).find('p').text();
-    $('.widway').val(paymentMethod);
-    backMethod();
-    
-})
-$('.chose-payment-back').click(function(){
-    backMethod();
-})
-
 // 发票
 $('.invoice-li').click(function(){
     $('.invoice-container').show();
