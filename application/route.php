83613a50691d1daf0d9d251a5239f95f82f40d63
lihuosheng-top <1061525487@qq.com>
2019-01-23 12:26:57 +0800

    laqu

--- application/route.php
+++ application/route.php
@@ -129,7 +129,6 @@ Route::group("",[
     "sell_my_index"=>"index/SellMy/sell_my_index",//卖家我的页面
     "store_order_save"=>"index/SellMy/store_order_save",//卖家暂存订单信息（方便点击到对应的订单信息）
     "store_order_get"=>"index/SellMy/store_order_get",//卖家读取信息（方便判断点击的是哪个按钮进来）
-
     "sell_service_order"=>"index/SellMy/sell_service_order",//卖家服务订单
     "sell_service_price_change"=>"index/SellMy/sell_service_price_change",//服务商面议修改价格
     "sell_order_service_no_pay_cancel"=>"index/SellMy/sell_order_service_no_pay_cancel",//卖家取消订单接口(ajax)
