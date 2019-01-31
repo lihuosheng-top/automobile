b59600f31b8e2176f4f43dc299fce783ef23025f
lihuosheng-top <1061525487@qq.com>
2019-01-31 16:25:15 +0800

    laqu

--- application/index/controller/Apppay.php
+++ application/index/controller/Apppay.php
@@ -981,9 +981,7 @@ class Apppay extends Controller
             include EXTEND_PATH."WxpayAPI/lib/Wxpayandroid.php";
             $data = Db::name('order_parts')->where('parts_order_number',$order_num)->find();
 //            $goods_name = $data['parts_goods_name'];    //商品名称
-            header("Content-Type:text/html; charset=utf-8");
-            $goods_name =substr($data['parts_goods_name'],0,3);
-            $goods_name = iconv("gb2312", "utf-8", $goods_name);
+            $goods_name =substr($data['parts_goods_name'],0,20);
             $order_number = $data['parts_order_number'];    //订单号
             $goods_pay_money =$data['order_real_pay'];     //支付金额
             $notify_url = config("url_domain.address")."wxpay_parts_notifyurl";//异步通知URL(更改支付状态)
