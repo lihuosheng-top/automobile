ece377b675df4d0fd88faeef1e710c31eaf56e12
guoyangzt <gy156789@163.com>
2019-02-28 10:15:34 +0800

    拉代码

--- application/index/controller/OrderParts.php
+++ application/index/controller/OrderParts.php
@@ -2143,7 +2143,6 @@ class OrderParts extends Controller{
                     "pay_type_content" => $pay_type_content,
                     "refund_amount" => $refund_amount
                 ];
-                halt($data);
                 $bool = Db::name("order_parts")->where("id", $id)->update($data);
                 if ($bool) {
                     if ($status == 3) {
