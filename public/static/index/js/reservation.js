b062b40a6f742a595853ad2b58a8b33c7226e4fc
youdianku <571851168@qq.com>
2019-03-01 15:43:00 +0800

    push

--- public/static/index/js/reservation.js
+++ public/static/index/js/reservation.js
@@ -20,7 +20,7 @@ AMap.plugin([
     map.addControl(new AMap.ToolBar());
     var geolocation = new AMap.Geolocation({
         enableHighAccuracy: true,
-        // timeout: 10000,
+        timeout: 10000,
         buttonPosition: 'RB',
         buttonOffset: new AMap.Pixel(10, 20),
         zoomToAccuracy: true
